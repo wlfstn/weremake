8a69fad9319a5f502a8a9fa4a0b3c1ec78b68683
CorpoWolf <gmangavin1@gmail.com>
2025-01-16 09:24:05 -0800

    flag parse added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module weremake
 
 go 1.23.4
+
+require github.com/BurntSushi/toml v1.4.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-				"fmt"
+	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
-				fmt.Println("Hello, this is weremake")
+	fmt.Println("Hello, this is weremake")
+
+	initFlag := flag.Bool("init", false, "Initialize the application")
+	flag.Parse()
+
+	weremakeFile := "weremake.toml"
+	if *initFlag {
+		if _, err := os.Stat(weremakeFile); os.IsNotExist(err) {
+			fmt.Printf("File '%s' not found. Please ensure it exists before proceeding.\n", weremakeFile)
+		}
+	}
 }
